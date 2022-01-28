4d909cd80f864e3b404218b826b3726d3a56d3d4
alexanderkotlouski <Katlouski@gmail.com>
2022-01-28 12:21:33 +0300

    Add 7-10

---
+++ 7-10.py
@@ -0,0 +1,6 @@
+print(my_dict['bar']['d'])
+my_dict['moo']['f']['Lol'].pop(1)
+print(my_dict['moo']['f']['Lol'])
+my_dict['moo']['f']['k'] = ['K', 'e', 'k']
+print(my_dict['moo']['f']['k'])
+my_dict.clear()
